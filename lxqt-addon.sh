4086ab8afa97f279eb6038d324c3c6bef5b59bf7
Gaston <93546109+gascodes@users.noreply.github.com>
2022-09-22 10:29:47 -0300

    Update lxqt-addon.sh

--- lxqt-addon.sh
+++ lxqt-addon.sh
@@ -1,38 +1,13 @@
 #!/bin/bash
-#set -e
-
-
-###############################################################################
-#
-#   DECLARATION OF FUNCTIONS
-#
-###############################################################################
-
-
-func_install() {
+func_instalar() {
 	if pacman -Qi $1 &> /dev/null; then
-		tput setaf 2
-  		echo "###############################################################################"
-  		echo "################## The package "$1" is already installed"
-      	echo "###############################################################################"
-      	echo
-		tput sgr0
 	else
-    	tput setaf 3
-    	echo "###############################################################################"
-    	echo "##################  Installing package "  $1
-    	echo "###############################################################################"
-    	echo
-    	tput sgr0
     	sudo pacman -S --noconfirm --needed $1 
     fi
 }
 
-###############################################################################
-echo "Installation of bluetooth software"
-###############################################################################
 
-list=(
+lista=(
 pulseaudio-bluetooth
 bluez
 bluez-libs
@@ -47,30 +22,18 @@ zsh-lovers
 gvfs
 gvfs-afc
 inter-font
+terminus-font
 catppuccin-gtk-theme
 )
 
 count=0
 
-for name in "${list[@]}" ; do
+for paquete in "${lista[@]}" ; do
 	count=$[count+1]
-	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
-	func_install $name
+	func_instalar $paquete
 done
 
-###############################################################################
-
-tput setaf 5;echo "################################################################"
-echo "Enabling services"
-echo "################################################################"
-echo;tput sgr0
-
+#Habilitando Servicios
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
 sudo sed -i 's/'#AutoEnable=false'/'AutoEnable=true'/g' /etc/bluetooth/main.conf
-
-tput setaf 11;
-echo "################################################################"
-echo "Software has been installed"
-echo "################################################################"
-echo;tput sgr0
